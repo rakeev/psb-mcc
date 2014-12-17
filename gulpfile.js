0828bfb00005b8feb8b81d99efa4f43724e5ff22
Alexey Rakeev <alexey@rakeev.name>
2014-12-17 05:05:51 +0400

    Simplified bonus detection

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-    bonus = require('./src/bonus'),
     uglify = require('uglify-js'),
     qs = require('querystring'),
     fs = require('fs');
 
 gulp.task('build', function() {
-    Object.keys(bonus).forEach(function(key) {
-       bonus[key] = uglify.parse(bonus[key].toString(), {expression: true}).print_to_string();
-    });
-    var code = 'var bonus = '+JSON.stringify(bonus);
-    code += ', script = "'+qs.escape(uglify.minify('src/script.js').code)+'";';
+    var code = 'var script = "'+qs.escape(uglify.minify('src/script.js').code)+'";';
     fs.existsSync('res') || fs.mkdirSync('res', '0755');
     fs.writeFile('res/data.js', code);
 });
--- src/script.js
+++ src/script.js
@@ -1,14 +1,36 @@
+var codes = {
+    'fuel': [5172, 5541, 5542, 5983],
+    'avia': ['3000-3299', 4112, 4411, 4511],
+    'groc': [5411, 5422, 5441, 5451, 5462, 5499, '5811-5814', 5921],
+    'garm': [5094, 5137, 5139, 5331, 5611, 5621, 5631, 5641, 5651,
+        5661, 5681, 5691, '5697-5699', 5931, 5944, 5949, 5950, 7296],
+    'home': [1520, 1711, 1731, 1740, 1750, 1761, 1771, 1799, 2791, 2842, 5021, 5039,
+        5046, 5051, 5065, 5072, 5074, 5085, 5198, 5200, 5211, 5231, 5251, 5261,
+        '5712-5714', 5718, 5719, 5722, 7622, 7623, 7629, 7631, 7641, 7692, 7699]
+};
+
+var isBonus = function(mcc) {
+    return $.grep(codes[cat], function(val, idx) {
+            var chunks = (val + '').split('-');
+            if (chunks.length > 1) {
+                return (mcc >= chunks[0]) && (mcc <= chunks[1]);
+            }
+            return (val == mcc);
+        }).length > 0;
+};
+
 var updateLayout = function(content) {
     $.each(content.data.Transactions, function(idx, val) {
         if (!val.Mcc) {
             return true;
         }
-        var row = $('*[data-id='+val.Id+']');
-        row.find('td[data-category] .data').prepend('<b>'+val.Mcc+'</b> ');
+        var row = $('*[data-id=' + val.Id + ']');
+        row.find('td[data-category] .data').prepend('<b>' + val.Mcc + '</b> ');
         if (isBonus(val.Mcc)) {
             row.find('td[data-amount]').prepend('<b style="color: green">+5%</b> ');
         }
     });
 };
+
 updateLayout(window.transactionsPage);
 Meniga.EventBus.bind(Meniga.Transactions.Events.TransactionsListLoaded, updateLayout);
