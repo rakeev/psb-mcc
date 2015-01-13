90a5628fb8a865c16ba76bcd770805be854c6fb2
Alexey Rakeev <alexey@rakeev.name>
2015-01-14 00:42:23 +0400

    Implemented watch task

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,26 @@ var gulp = require('gulp'),
     plug = require('gulp-load-plugins')(),
     qs = require('querystring');
 
-gulp.task('build', function() {
+var path = 'res',
+    scripts = './src/*.js',
+    styles = ['node_modules/picnic/src/picnic.scss', './src/*.scss'];
+
+gulp.task('js', function() {
     var trans = plug.insert.transform(function(cont) {
         return 'var code = "'+qs.escape(cont)+'";';
     });
-    gulp.src('./src/*.js').pipe(plug.uglify()).pipe(plug.if(/main\.js$/, trans))
-        .pipe(plug.concat('script.js')).pipe(gulp.dest('res'));
-
-    gulp.src('node_modules/picnic/src/picnic.scss').pipe(plug.sass()).pipe(plug.rename('style.css'))
-        .pipe(plug.autoprefixer()).pipe(plug.minifyCss()).pipe(gulp.dest('res'));
+    gulp.src(scripts).pipe(plug.uglify()).pipe(plug.if(/main\.js$/, trans))
+        .pipe(plug.concat('script.js')).pipe(gulp.dest(path)).pipe(plug.notify('Rebuilt js'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('css', function() {
+    gulp.src(styles).pipe(plug.sass()).pipe(plug.rename('style.css')).pipe(plug.autoprefixer())
+        .pipe(plug.minifyCss()).pipe(gulp.dest(path)).pipe(plug.notify('Rebuilt css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(scripts, ['js']);
+    gulp.watch(styles, ['css']);
+});
+
+gulp.task('default', ['js', 'css']);
